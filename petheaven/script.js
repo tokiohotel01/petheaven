fce0bee72bacf8f23a82210528841f9df42b30b9
tokiohotel01 <135271402+tokiohotel01@users.noreply.github.com>
2024-12-17 17:49:05 -0300

    Add files via upload

---
+++ petheaven/script.js
@@ -0,0 +1,4 @@
+// Botón "Explorar Servicios"
+document.querySelector('.btn').addEventListener('click', function() {
+    alert('¡Explora todos nuestros servicios para consentir a tu mascota!');
+});
